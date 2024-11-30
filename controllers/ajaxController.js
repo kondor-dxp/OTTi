0597ce8fb8c176cee630dea7a478af95b96b22f8
Dario <dariomunao@gmail.com>
2024-11-30 15:18:56 +0100

    Inizializzato controller ajax per eventuali chiamate ajax

---
+++ controllers/ajaxController.js
@@ -0,0 +1,39 @@
+const functions = require('../db/functions')
+
+const ajaxController = {
+    async home(req, res) {
+
+        // VARIABILI COMUNI A TUTTI I CONTROLLER
+        const route = "home"
+        const session = require('../controllers/sessionController');
+
+        // VARIABILI SPECIFICHE DEL CONTROLLER
+        ajax = req.params.ajax
+
+        switch(ajax){
+            case "addUser":{
+                const data = req.body
+                try {
+                    const result = await functions.add("User", data)
+                    
+                    // RESTITUZIONE DEL JSON ELABORATO
+                    const status = result ? 200 : 400
+                    res.status(status).json(data)
+                } catch (error) {
+
+                    // GESTIONE DEGLI ERRORI DELLA CHIAMATA AJAX
+                    const status = 500
+                    res.status(status).json(error)
+                }
+                break
+            }
+            default:
+
+                // RENDER DELLA PAGINA HTML SENZA DATI
+                let page = {  }
+                res.render('template', { page, route, session })
+                break
+        }
+    }
+}
+module.exports = ajaxController
