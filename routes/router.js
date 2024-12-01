3ac2393b435180e36d29674d0902a885882e655a
Dario <dariomunao@gmail.com>
2024-12-01 14:45:33 +0100

    Aggiunti controller e route ajax

--- routes/router.js
+++ routes/router.js
@@ -2,9 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 // DEFINIZIONE DEL CONTROLLER
+const ajaxController  = require('./../controllers/ajaxController')
 const integrixController = require('./../controllers/integrixController')
 
 // DEFINIZIONE DELLE ROUTE
+
+//ROUTE AJAX
+router.post('/ajax/:ajax', ajaxController.call)
 
 //ROUTE FRONTEND
 router.get('/', integrixController.home)
